1cf453e4d5272c2bd74e3afa31059b197286f248
Alex Kuhn <kuhn.alexey@gmail.com>
2025-08-03 15:00:18 +0300

    split init.lua into separated lua files
    kickstart.nvim installed all in a single file which is hard to navigate
    and update. TODO: remove unused plugins under lua/kickstart

---
+++ lua/plugins/lazygit.lua
@@ -0,0 +1,29 @@
+return {
+  -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
+  -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
+  -- Or use telescope!
+  -- In normal mode type `<space>sh` then write `lazy.nvim-plugin`
+  -- you can continue same window with `<space>sr` which resumes last telescope search
+  -- LazyGit plugin https://github.com/kdheepak/lazygit.nvim
+  {
+    'kdheepak/lazygit.nvim',
+    lazy = true,
+    cmd = {
+      'LazyGit',
+      'LazyGitConfig',
+      'LazyGitCurrentFile',
+      'LazyGitFilter',
+      'LazyGitFilterCurrentFile',
+    },
+    -- optional for floating window border decoration
+    dependencies = {
+      'nvim-telescope/telescope.nvim',
+      'nvim-lua/plenary.nvim',
+    },
+    -- setting the keybinding for LazyGit with 'keys' is recommended in
+    -- order to load the plugin when the command is run for the first time
+    keys = {
+      { '<leader>lg', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
+    },
+  },
+}
