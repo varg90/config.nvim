dd8dabd67781435f27c4e3b7943bda4f9a8eb7ef
Alex Kuhn <kuhn.alexey@gmail.com>
2025-09-01 22:28:42 +0300

    remove comments from lsp config

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,97 +1,125 @@
 return {
   {
-    -- Main LSP Configuration
     'neovim/nvim-lspconfig',
     dependencies = {
-      -- Automatically install LSPs and related tools to stdpath for Neovim
-      -- Mason must be loaded before its dependents so we need to set it up here.
-      -- NOTE: `opts = {}` is the same as calling `require('mason').setup({})`
-      { 'mason-org/mason.nvim', opts = { ensure_installed = { 'prettier', 'solargraph', 'eslint', 'tailwindcss', 'ts_ls' } } },
-      { 'mason-org/mason-lspconfig.nvim', opts = { ensure_installed = { 'solargraph', 'eslint' } } },
+      {
+        'mason-org/mason.nvim',
+        opts = {
+          ensure_installed = {
+            'prettier',
+            'solargraph',
+            'eslint',
+            'tailwindcss',
+            'ts_ls',
+          },
+        },
+      },
+      {
+        'mason-org/mason-lspconfig.nvim',
+        opts = { ensure_installed = { 'solargraph', 'eslint' } },
+      },
       'WhoIsSethDaniel/mason-tool-installer.nvim',
-
-      -- Useful status updates for LSP.
       { 'j-hui/fidget.nvim', opts = {} },
     },
     config = function()
-      -- Brief aside: **What is LSP?**
-      --
-      -- LSP is an initialism you've probably heard, but might not understand what it is.
-      --
-      -- LSP stands for Language Server Protocol. It's a protocol that helps editors
-      -- and language tooling communicate in a standardized fashion.
-      --
-      -- In general, you have a "server" which is some tool built to understand a particular
-      -- language (such as `gopls`, `lua_ls`, `rust_analyzer`, etc.). These Language Servers
-      -- (sometimes called LSP servers, but that's kind of like ATM Machine) are standalone
-      -- processes that communicate with some "client" - in this case, Neovim!
-      --
-      -- LSP provides Neovim with features like:
-      --  - Go to definition
-      --  - Find references
-      --  - Autocompletion
-      --  - Symbol Search
-      --  - and more!
-      --
-      -- Thus, Language Servers are external tools that must be installed separately from
-      -- Neovim. This is where `mason` and related plugins come into play.
-      --
-      -- If you're wondering about lsp vs treesitter, you can check out the wonderfully
-      -- and elegantly composed help section, `:help lsp-vs-treesitter`
-
-      --  This function gets run when an LSP attaches to a particular buffer.
-      --    That is to say, every time a new file is opened that is associated with
-      --    an lsp (for example, opening `main.rs` is associated with `rust_analyzer`) this
-      --    function will be executed to configure the current buffer
       vim.api.nvim_create_autocmd('LspAttach', {
-        group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
+        group = vim.api.nvim_create_augroup(
+          'kickstart-lsp-attach',
+          { clear = true }
+        ),
         callback = function(event)
           local bufnr = event.buf
 
-          vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { buffer = bufnr, remap = false, desc = '[LSP] Go to definition' })
-          vim.keymap.set('n', 'K', vim.lsp.buf.hover, { buffer = bufnr, remap = false, desc = '[LSP] Hover' })
-          vim.keymap.set('n', 'gs', vim.lsp.buf.references, { buffer = bufnr, remap = false, desc = '[LSP] Finder' })
-
-          vim.keymap.set('n', 'gi', vim.lsp.buf.type_definition, { buffer = bufnr, remap = false, desc = '[LSP] Hover doc' })
-          vim.keymap.set({ 'n', 'v' }, '<leader>va', vim.lsp.buf.code_action, { buffer = bufnr, remap = false, desc = '[LSP] Code actions' })
-          vim.keymap.set('n', '<leader>vws', function()
-            vim.lsp.buf.workspace_symbol()
-          end, { buffer = bufnr, remap = false, desc = '[LSP] Workspace symbol' })
+          vim.keymap.set(
+            'n',
+            'gd',
+            vim.lsp.buf.definition,
+            { buffer = bufnr, remap = false, desc = '[LSP] Go to definition' }
+          )
+          vim.keymap.set(
+            'n',
+            'K',
+            vim.lsp.buf.hover,
+            { buffer = bufnr, remap = false, desc = '[LSP] Hover' }
+          )
+          vim.keymap.set(
+            'n',
+            'gs',
+            vim.lsp.buf.references,
+            { buffer = bufnr, remap = false, desc = '[LSP] Finder' }
+          )
+          vim.keymap.set(
+            'n',
+            'gi',
+            vim.lsp.buf.type_definition,
+            { buffer = bufnr, remap = false, desc = '[LSP] Hover doc' }
+          )
+          vim.keymap.set(
+            { 'n', 'v' },
+            '<leader>va',
+            vim.lsp.buf.code_action,
+            { buffer = bufnr, remap = false, desc = '[LSP] Code actions' }
+          )
+          vim.keymap.set(
+            'n',
+            '<leader>vws',
+            function()
+              vim.lsp.buf.workspace_symbol()
+            end,
+            { buffer = bufnr, remap = false, desc = '[LSP] Workspace symbol' }
+          )
           vim.keymap.set('n', '<leader>vh', function()
             vim.diagnostic.open_float()
-          end, { buffer = bufnr, remap = false, desc = '[LSP] Open float' })
-
+          end, {
+            buffer = bufnr,
+            remap = false,
+            desc = '[LSP] Open float',
+          })
           vim.keymap.set('n', '[d', function()
             vim.diagnostic.goto_next()
-          end, { buffer = bufnr, remap = false, desc = '[LSP] Goto next' })
-
+          end, {
+            buffer = bufnr,
+            remap = false,
+            desc = '[LSP] Goto next',
+          })
           vim.keymap.set('n', ']d', function()
             vim.diagnostic.goto_prev()
-          end, { buffer = bufnr, remap = false, desc = '[LSP] Goto prev' })
-
+          end, {
+            buffer = bufnr,
+            remap = false,
+            desc = '[LSP] Goto prev',
+          })
           vim.keymap.set('n', '<leader>ve', function()
             vim.lsp.buf.references()
-          end, { buffer = bufnr, remap = false, desc = '[LSP] References' })
-
+          end, {
+            buffer = bufnr,
+            remap = false,
+            desc = '[LSP] References',
+          })
           vim.keymap.set('n', 'gr', function()
             vim.lsp.buf.rename()
           end, { buffer = bufnr, remap = false, desc = '[LSP] Rename' })
-
           vim.keymap.set('i', '<c-h>', function()
             vim.lsp.buf.signature_help()
-          end, { buffer = bufnr, remap = false, desc = '[LSP] Signature help' })
-
-          vim.keymap.set('n', '<leader>vo', function()
-            vim.lsp.buf.execute_command {
-              command = '_typescript.organizeImports',
-              arguments = { vim.fn.expand '%:p' },
-            }
-          end, { buffer = bufnr, remap = false, desc = '[LSP] Organize imports' })
+          end, {
+            buffer = bufnr,
+            remap = false,
+            desc = '[LSP] Signature help',
+          })
+          vim.keymap.set(
+            'n',
+            '<leader>vo',
+            function()
+              vim.lsp.buf.execute_command {
+                command = '_typescript.organizeImports',
+                arguments = { vim.fn.expand '%:p' },
+              }
+            end,
+            { buffer = bufnr, remap = false, desc = '[LSP] Organize imports' }
+          )
         end,
       })
 
-      -- Diagnostic Config
-      -- See :help vim.diagnostic.Opts
       vim.diagnostic.config {
         severity_sort = true,
         float = { border = 'rounded', source = 'if_many' },
@@ -119,28 +147,7 @@ return {
         },
       }
 
-      -- Enable the following language servers
-      --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
-      --
-      --  Add any additional override configuration in the following tables. Available keys are:
-      --  - cmd (table): Override the default command used to start the server
-      --  - filetypes (table): Override the default list of associated filetypes for the server
-      --  - capabilities (table): Override fields in capabilities. Can be used to disable certain LSP features.
-      --  - settings (table): Override the default settings passed when initializing the server.
-      --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        -- clangd = {},
-        -- gopls = {},
-        -- pyright = {},
-        -- rust_analyzer = {},
-        -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-        --
-        -- Some languages (like typescript) have entire language plugins that can be useful:
-        --    https://github.com/pmizio/typescript-tools.nvim
-        --
-        -- But for many setups, the LSP (`ts_ls`) will work just fine
-        -- ts_ls = {},
-        --
         solargraph = {
           cmd = (function()
             local util = require 'lspconfig.util'
@@ -156,44 +163,29 @@ return {
               diagnostics = true,
               formatting = true,
               completion = true,
-              useBundler = false, -- we override `cmd`, so disable Solargraph's internal bundler logic
+              useBundler = false,
             },
           },
         },
         lua_ls = {
-          -- cmd = { ... },
-          -- filetypes = { ... },
-          -- capabilities = {},
           settings = {
             Lua = {
               completion = {
                 callSnippet = 'Replace',
               },
-              -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-              -- diagnostics = { disable = { 'missing-fields' } },
             },
           },
         },
       }
 
-      -- Ensure the servers and tools above are installed
-      --
-      -- To check the current status of installed tools and/or manually install
-      -- other tools, you can run
-      --    :Mason
-      --
-      -- You can press `g?` for help in this menu.
-      --
-      -- `mason` had to be setup earlier: to configure its options see the
-      -- `dependencies` table for `nvim-lspconfig` above.
-      --
-      -- You can add other tools here that you want Mason to install
-      -- for you, so that they are available from within Neovim.
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
-        'stylua', -- Used to format Lua code
+        'stylua',
       })
-      require('mason-tool-installer').setup { ensure_installed = ensure_installed }
+
+      require('mason-tool-installer').setup {
+        ensure_installed = ensure_installed,
+      }
 
       require('mason-lspconfig').setup {
         ensure_installed = {
@@ -211,10 +203,13 @@ return {
         handlers = {
           function(server_name)
             local server = servers[server_name] or {}
-            -- This handles overriding only values explicitly passed
-            -- by the server configuration above. Useful when disabling
-            -- certain features of an LSP (for example, turning off formatting for ts_ls)
-            server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
+
+            server.capabilities = vim.tbl_deep_extend(
+              'force',
+              {},
+              capabilities,
+              server.capabilities or {}
+            )
             require('lspconfig')[server_name].setup(server)
           end,
         },
