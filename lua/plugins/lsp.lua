e8b3cc53fd8f4bf249a42559974782d8edd4570c
Alex Kuhn <kuhn.alexey@gmail.com>
2025-09-01 23:43:21 +0300

    replace solargraph with ruby-lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,122 +1,97 @@
 return {
   {
+
     'neovim/nvim-lspconfig',
     dependencies = {
       {
         'mason-org/mason.nvim',
         opts = {
-          ensure_installed = {
-            'prettier',
-            'solargraph',
-            'eslint',
-            'tailwindcss',
-            'ts_ls',
-          },
+          ensure_installed = { 'prettier', 'eslint', 'tailwindcss', 'ts_ls' },
         },
       },
       {
         'mason-org/mason-lspconfig.nvim',
-        opts = { ensure_installed = { 'solargraph', 'eslint' } },
+        opts = { ensure_installed = { 'eslint' } },
       },
       'WhoIsSethDaniel/mason-tool-installer.nvim',
       { 'j-hui/fidget.nvim', opts = {} },
     },
     config = function()
+      local cmp_nvim_lsp = require 'cmp_nvim_lsp'
+      local capabilities = vim.tbl_deep_extend(
+        'force',
+        {},
+        vim.lsp.protocol.make_client_capabilities(),
+        cmp_nvim_lsp.default_capabilities()
+      )
+
+      require('lspconfig').ruby_lsp.setup {
+        cmd = (function()
+          local root = vim.loop.cwd()
+          if vim.uv.fs_stat(root .. '/Gemfile') then
+            return { 'bundle', 'exec', 'ruby-lsp' }
+          else
+            return { 'ruby-lsp' }
+          end
+        end)(),
+        capabilities = capabilities,
+      }
+
       vim.api.nvim_create_autocmd('LspAttach', {
-        group = vim.api.nvim_create_augroup(
-          'kickstart-lsp-attach',
-          { clear = true }
-        ),
+        group = vim.api.nvim_create_augroup('lsp-attach', { clear = true }),
         callback = function(event)
           local bufnr = event.buf
+          local opts = function(desc)
+            return { buffer = bufnr, remap = false, desc = desc }
+          end
 
           vim.keymap.set(
             'n',
             'gd',
             vim.lsp.buf.definition,
-            { buffer = bufnr, remap = false, desc = '[LSP] Go to definition' }
+            opts '[LSP] Go to definition'
           )
-          vim.keymap.set(
-            'n',
-            'K',
-            vim.lsp.buf.hover,
-            { buffer = bufnr, remap = false, desc = '[LSP] Hover' }
-          )
-          vim.keymap.set(
-            'n',
-            'gs',
-            vim.lsp.buf.references,
-            { buffer = bufnr, remap = false, desc = '[LSP] Finder' }
-          )
+          vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts '[LSP] Hover')
+          vim.keymap.set('n', 'gs', vim.lsp.buf.references, opts '[LSP] Finder')
           vim.keymap.set(
             'n',
             'gi',
             vim.lsp.buf.type_definition,
-            { buffer = bufnr, remap = false, desc = '[LSP] Hover doc' }
+            opts '[LSP] Hover doc'
           )
           vim.keymap.set(
             { 'n', 'v' },
             '<leader>va',
             vim.lsp.buf.code_action,
-            { buffer = bufnr, remap = false, desc = '[LSP] Code actions' }
+            opts '[LSP] Code actions'
           )
-          vim.keymap.set(
-            'n',
-            '<leader>vws',
-            function()
-              vim.lsp.buf.workspace_symbol()
-            end,
-            { buffer = bufnr, remap = false, desc = '[LSP] Workspace symbol' }
-          )
+          vim.keymap.set('n', '<leader>vws', function()
+            vim.lsp.buf.workspace_symbol()
+          end, opts '[LSP] Workspace symbol')
           vim.keymap.set('n', '<leader>vh', function()
             vim.diagnostic.open_float()
-          end, {
-            buffer = bufnr,
-            remap = false,
-            desc = '[LSP] Open float',
-          })
+          end, opts '[LSP] Open float')
           vim.keymap.set('n', '[d', function()
             vim.diagnostic.goto_next()
-          end, {
-            buffer = bufnr,
-            remap = false,
-            desc = '[LSP] Goto next',
-          })
+          end, opts '[LSP] Goto next')
           vim.keymap.set('n', ']d', function()
             vim.diagnostic.goto_prev()
-          end, {
-            buffer = bufnr,
-            remap = false,
-            desc = '[LSP] Goto prev',
-          })
+          end, opts '[LSP] Goto prev')
           vim.keymap.set('n', '<leader>ve', function()
             vim.lsp.buf.references()
-          end, {
-            buffer = bufnr,
-            remap = false,
-            desc = '[LSP] References',
-          })
+          end, opts '[LSP] References')
           vim.keymap.set('n', 'gr', function()
             vim.lsp.buf.rename()
-          end, { buffer = bufnr, remap = false, desc = '[LSP] Rename' })
+          end, opts '[LSP] Rename')
           vim.keymap.set('i', '<c-h>', function()
             vim.lsp.buf.signature_help()
-          end, {
-            buffer = bufnr,
-            remap = false,
-            desc = '[LSP] Signature help',
-          })
-          vim.keymap.set(
-            'n',
-            '<leader>vo',
-            function()
-              vim.lsp.buf.execute_command {
-                command = '_typescript.organizeImports',
-                arguments = { vim.fn.expand '%:p' },
-              }
-            end,
-            { buffer = bufnr, remap = false, desc = '[LSP] Organize imports' }
-          )
+          end, opts '[LSP] Signature help')
+          vim.keymap.set('n', '<leader>vo', function()
+            vim.lsp.buf.execute_command {
+              command = '_typescript.organizeImports',
+              arguments = { vim.fn.expand '%:p' },
+            }
+          end, opts '[LSP] Organize imports')
         end,
       })
 
@@ -148,41 +123,20 @@ return {
       }
 
       local servers = {
-        solargraph = {
-          cmd = (function()
-            local util = require 'lspconfig.util'
-            local root = vim.fn.getcwd()
-            if util.path.exists(util.path.join(root, 'Gemfile')) then
-              return { 'bundle', 'exec', 'solargraph', 'stdio' }
-            else
-              return { 'solargraph', 'stdio' }
-            end
-          end)(),
-          settings = {
-            solargraph = {
-              diagnostics = true,
-              formatting = true,
-              completion = true,
-              useBundler = false,
-            },
-          },
+        ruby_lsp = {
+          mason = false,
         },
         lua_ls = {
           settings = {
             Lua = {
-              completion = {
-                callSnippet = 'Replace',
-              },
+              completion = { callSnippet = 'Replace' },
             },
           },
         },
       }
 
       local ensure_installed = vim.tbl_keys(servers or {})
-      vim.list_extend(ensure_installed, {
-        'stylua',
-      })
-
+      vim.list_extend(ensure_installed, { 'stylua' })
       require('mason-tool-installer').setup {
         ensure_installed = ensure_installed,
       }
@@ -195,7 +149,6 @@ return {
           'ember',
           'rust_analyzer',
           'lua_ls',
-          'solargraph',
           'emmet_ls',
           'elixirls',
         },
@@ -203,7 +156,6 @@ return {
         handlers = {
           function(server_name)
             local server = servers[server_name] or {}
-
             server.capabilities = vim.tbl_deep_extend(
               'force',
               {},
