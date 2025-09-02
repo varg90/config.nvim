f4b8b7cecfc5c8eadd38abfbe426d872d76d04fa
Alex Kuhn <kuhn.alexey@gmail.com>
2025-09-02 15:44:57 +0300

    vim-rspec hotkeys

---
+++ lua/plugins/testing.lua
@@ -0,0 +1,33 @@
+return {
+  'thoughtbot/vim-rspec',
+  dependencies = { 'tpope/vim-rails', 'preservim/vimux' },
+  config = function()
+    vim.api.nvim_set_keymap(
+      'n',
+      '<leader>tt',
+      ':A<CR>',
+      { noremap = true, silent = true, desc = '[T]oggle Spec File' }
+    )
+    vim.api.nvim_set_keymap(
+      'n',
+      '<leader>tr',
+      ':call RunNearestSpec()<CR>',
+      { noremap = true, silent = true, desc = '[T]est [R]un Nearest' }
+    )
+    vim.api.nvim_set_keymap(
+      'n',
+      '<leader>tf',
+      ':call RunCurrentSpecFile()<CR>',
+      { noremap = true, silent = true, desc = '[T]est [F]ile' }
+    )
+    vim.api.nvim_set_keymap(
+      'n',
+      '<leader>tl',
+      ':call RunLastSpec()<CR>',
+      { noremap = true, silent = true, desc = '[T]est [L]ast' }
+    )
+
+    vim.g.rspec_command = '!bundle exec rspec {spec}'
+    vim.g.rspec_runner = 'vimux'
+  end,
+}
