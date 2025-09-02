a3edbd1bb8c2d4c49d39e6c3b119b6af82c14100
Alex Kuhn <kuhn.alexey@gmail.com>
2025-09-02 15:42:47 +0300

    show hidden files in oil

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -3,18 +3,27 @@ return {
   ---@module 'oil'
   ---@type oil.SetupOpts
   opts = {
-      view_options = {
-          -- Show files and directories that start with "."
-          show_hidden = true,
-      },
+    view_options = {
+      -- Show files and directories that start with "."
+      show_hidden = true,
+      is_always_hidden = function(name, _bufnr)
+        local hide = {
+          '.git',
+          '.github',
+          '.idea',
+          '.ruby-lsp',
+        }
+        return vim.tbl_contains(hide, name)
+      end,
+    },
   },
   -- Optional dependencies
   -- dependencies = { { 'echasnovski/mini.icons', opts = {} } },
   dependencies = { { 'nvim-tree/nvim-web-devicons', opts = {} } }, -- use if you prefer nvim-web-devicons
   -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
   lazy = false,
-  config = function()
-    require('oil').setup {}
+  config = function(_, opts)
+    require('oil').setup(opts)
 
     vim.api.nvim_create_autocmd('FileType', {
       pattern = 'oil',
