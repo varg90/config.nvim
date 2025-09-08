637f54b1caa9d412e6c9ae0ff1ff419b3ecd9a86
Alex Kuhn <kuhn.alexey@gmail.com>
2025-09-08 10:07:38 +0300

    add git blame and git preview_hunk hotkeys

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,6 +1,14 @@
 return {
   {
     'tpope/vim-fugitive',
+    config = function()
+      vim.keymap.set(
+        'n',
+        '<leader>gb',
+        ':Git blame<CR>',
+        { desc = '[G]it [B]lame' }
+      )
+    end,
   },
   {
     'lewis6991/gitsigns.nvim',
@@ -13,5 +21,17 @@ return {
         changedelete = { text = '~' },
       },
     },
+    config = function()
+      require('gitsigns').setup {
+        on_attach = function(bufnr)
+          vim.keymap.set('n', '<leader>gp', ':Gitsigns preview_hunk<CR>', {
+            buffer = bufnr,
+            noremap = true,
+            silent = true,
+            desc = '[G]it [P]review hunk',
+          })
+        end,
+      }
+    end,
   },
 }
