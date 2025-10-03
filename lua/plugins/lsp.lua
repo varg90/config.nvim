74e45dfcc91e4e58496b3defa82244797057015b
Alex Kuhn <kuhn.alexey@gmail.com>
2025-10-03 14:21:39 +0300

    revert solargraph

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,17 +1,22 @@
 return {
   {
-
     'neovim/nvim-lspconfig',
     dependencies = {
       {
         'mason-org/mason.nvim',
         opts = {
-          ensure_installed = { 'prettier', 'eslint', 'tailwindcss', 'ts_ls' },
+          ensure_installed = {
+            'prettier',
+            'solargraph',
+            'eslint',
+            'tailwindcss',
+            'ts_ls',
+          },
         },
       },
       {
         'mason-org/mason-lspconfig.nvim',
-        opts = { ensure_installed = { 'eslint' } },
+        opts = { ensure_installed = { 'solargraph', 'eslint' } },
       },
       'WhoIsSethDaniel/mason-tool-installer.nvim',
       { 'j-hui/fidget.nvim', opts = {} },
@@ -25,17 +30,17 @@ return {
         cmp_nvim_lsp.default_capabilities()
       )
 
-      require('lspconfig').ruby_lsp.setup {
-        cmd = (function()
-          local root = vim.loop.cwd()
-          if vim.uv.fs_stat(root .. '/Gemfile') then
-            return { 'bundle', 'exec', 'ruby-lsp' }
-          else
-            return { vim.fn.expand '$HOME/.rbenv/shims/ruby-lsp' }
-          end
-        end)(),
-        capabilities = capabilities,
-      }
+      -- require('lspconfig').ruby_lsp.setup {
+      --   cmd = (function()
+      --     local root = vim.loop.cwd()
+      --     if vim.uv.fs_stat(root .. '/Gemfile') then
+      --       return { 'bundle', 'exec', 'ruby-lsp' }
+      --     else
+      --       return { vim.fn.expand '$HOME/.rbenv/shims/ruby-lsp' }
+      --     end
+      --   end)(),
+      --   capabilities = capabilities,
+      -- }
 
       vim.api.nvim_create_autocmd('LspAttach', {
         group = vim.api.nvim_create_augroup('lsp-attach', { clear = true }),
@@ -123,8 +128,27 @@ return {
       }
 
       local servers = {
-        ruby_lsp = {
-          mason = false,
+        -- ruby_lsp = {
+        --   mason = false,
+        -- },
+        solargraph = {
+          cmd = (function()
+            local util = require 'lspconfig.util'
+            local root = vim.fn.getcwd()
+            if util.path.exists(util.path.join(root, 'Gemfile')) then
+              return { 'bundle', 'exec', 'solargraph', 'stdio' }
+            else
+              return { 'solargraph', 'stdio' }
+            end
+          end)(),
+          settings = {
+            solargraph = {
+              diagnostics = true,
+              formatting = true,
+              completion = true,
+              useBundler = false,
+            },
+          },
         },
         lua_ls = {
           settings = {
