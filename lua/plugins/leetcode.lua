ffb8970e71b531e39cc0e5074704c17718137651
Alex Kuhn <kuhn.alexey@gmail.com>
2025-10-08 18:19:12 +0300

    Revert "remove codeium and windsurf"
    This reverts commit 5e4f74350d29a0b1d4bd9e0941da7cb02717de85.

---
+++ lua/plugins/leetcode.lua
@@ -0,0 +1,23 @@
+return {
+  'kawre/leetcode.nvim',
+  build = ':TSUpdate html',
+  dependencies = {
+    'nvim-telescope/telescope.nvim', -- optional, but useful
+    'nvim-lua/plenary.nvim',
+    'MunifTanjim/nui.nvim',
+  },
+  opts = {
+    picker = { provider = 'telescope' },
+    lang = 'ruby', -- or "python", "cpp", etc.
+    keys = {
+      { '<leader>lt', ':LeetCodeTest<CR>', desc = 'LeetCode: Test code' },
+      { '<leader>lr', ':LeetCodeRun<CR>', desc = 'LeetCode: Run code' },
+      {
+        '<leader>ls',
+        ':LeetCodeSubmit<CR>',
+        desc = 'LeetCode: Submit solution',
+      },
+    },
+  },
+  lazy = false,
+}
