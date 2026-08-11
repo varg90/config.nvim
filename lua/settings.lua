b3c144bb102242e93990529abe529333e125e453
Alex Kuhn <kun.aa@insales.ru>
2026-08-11 20:38:27 +0300

    Enable russian spellcheck, ignore generated spell files

--- lua/settings.lua
+++ lua/settings.lua
@@ -99,7 +99,7 @@ vim.opt.backup = false
 
 vim.opt.spell = true
 vim.opt.spelloptions = 'camel'
-vim.opt.spelllang = 'en_us'
+vim.opt.spelllang = 'en_us,ru'
 
 vim.opt.langmap = [[фa,иb,сc,вd,уe,аf,пg,рh,шi,оj,лk,дl,ьm,тn,щo,зp,йq,кr,ыs,еt,гu,мv,цw,чx,нy,яz,ФA,ИB,СC,ВD,УE,АF,ПG,РH,ШI,ОJ,ЛK,ДL,ЬM,ТN,ЩO,ЗP,ЙQ,КR,ЫS,ЕT,ГU,МV,ЦW,ЧX,НY,ЯZ]]
 
