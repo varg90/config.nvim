a86237292fde917e254b6cfd710c893c05ecd34a
Alex Kuhn <kun.aa@insales.ru>
2026-09-02 16:00:12 +0300

    Make indent guides thinner and dimmer

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -3,5 +3,13 @@ return {
   main = 'ibl',
   ---@module "ibl"
   ---@type ibl.config
-  opts = {},
+  opts = {
+    indent = { char = '▏', highlight = 'IblIndent' },
+    scope = { char = '▏', highlight = 'IblScope' },
+  },
+  config = function(_, opts)
+    vim.api.nvim_set_hl(0, 'IblIndent', { fg = '#363636' })
+    vim.api.nvim_set_hl(0, 'IblScope', { fg = '#484848' })
+    require('ibl').setup(opts)
+  end,
 }
